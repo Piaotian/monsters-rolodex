443df697da0f1d26e2bbc9db44815b81b38de3fd
Piaotian <piaoapiaotian@gmail.com>
2020-05-25 15:05:28 -0400

    Updates

---
+++ precache-manifest.ee7250319dce6005254c530df2d5d7af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77f9af96ba7535cf40c09da253c1256",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f8ffb64a0ddaebc7254",
+    "url": "/monsters-rolodex/static/css/main.e43d5812.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8ffb64a0ddaebc7254",
+    "url": "/monsters-rolodex/static/js/main.705824e0.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
